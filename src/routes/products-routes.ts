0255aab0c2d939e9a93f5cb578ef31962a8e8ee8
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-02-07 09:48:14 -0300

    feat: separando as rotas

---
+++ src/routes/products-routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { myMiddleware } from "../middlewares/my-middleware";
+
+const productsRoutes = Router()
+
+
+
+productsRoutes.get("/products/:id", (request, response) => {
+	//    /products?page=1&limit=10
+	// const {page, limit} = req.query  // pegar os query params 
+	const { id } = request.params
+	response.send(`Hello World Express! ${id}`)
+})
+
+// colocar a função do middleware antes da funcao da rota
+
+productsRoutes.post("/products", myMiddleware,(request, response) => {
+	const { name, price } = request.body
+
+	// response.send(`Nome do Produto é : ${name} Custa ${price}`) dessa forma a resposta
+	// chega como um texto
+
+	response.status(201).json({ name, price }) // dessa forma ja trasforma a res em json
+})
--- src/server.ts
+++ src/server.ts
@@ -14,23 +14,7 @@ app.use(myMiddleware) // usar um middleware de forma global
 
 
 
-app.get("/products/:id", (request, response) => {
-	//    /products?page=1&limit=10
-	// const {page, limit} = req.query  // pegar os query params 
-	const { id } = request.params
-	response.send(`Hello World Express! ${id}`)
-})
 
-// colocar a função do middleware antes da funcao da rota
-
-app.post("/products", myMiddleware,(request, response) => {
-	const { name, price } = request.body
-
-	// response.send(`Nome do Produto é : ${name} Custa ${price}`) dessa forma a resposta
-	// chega como um texto
-
-	response.status(201).json({ name, price }) // dessa forma ja trasforma a res em json
-})
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
