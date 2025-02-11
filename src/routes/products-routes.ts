6c0735e8a40df9163402bea7ac379cbdba387493
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-02-11 09:02:56 -0300

    feat: movendo função para controler

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,24 +1,12 @@
 import { Router } from "express";
 import { myMiddleware } from "../middlewares/my-middleware";
+import { ProductsController } from "../controllers/ProductsController";
 
 export const productsRoutes = Router()
+const productsControler = new ProductsController()
 
 
 
-productsRoutes.get("/:id", (request, response) => {
-	//    /products?page=1&limit=10
-	// const {page, limit} = req.query  // pegar os query params 
-	const { id } = request.params
-	response.send(`Hello World Express! ${id}`)
-})
+productsRoutes.get("/", productsControler.index)
 
-// colocar a função do middleware antes da funcao da rota
-
-productsRoutes.post("/", myMiddleware, (request, response) => {
-	const { name, price } = request.body
-
-	// response.send(`Nome do Produto é : ${name} Custa ${price}`) dessa forma a resposta
-	// chega como um texto
- 
-	response.status(201).json({ name, price }) // dessa forma ja trasforma a res em json
-}) 
+productsRoutes.post("/", myMiddleware, productsControler.create) 
