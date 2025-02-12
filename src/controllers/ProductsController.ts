0090365fcc15b607574b02e01f280c9bd62f070b
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-02-12 10:26:21 -0300

    feat: atualizando schema de validação zod

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
-import { AppError } from "../utils/AppError";
-import { z } from 'zod'
+import { Request, Response } from 'express';
+import { AppError } from '../utils/AppError';
+import { z } from 'zod';
 export class ProductsController {
 	/**
 	 * idex - GET para listar vários registros.
@@ -10,20 +10,25 @@ export class ProductsController {
 	 * remove - DELETE para remover um registro
 	 */
 
-
 	index(request: Request, response: Response) {
 		//    /products?page=1&limit=10
-		// const {page, limit} = request.query  // pegar os query params 
+		// const {page, limit} = request.query  // pegar os query params
 		// const { id } = request.params
-		response.send(`Hello World Expresss!`)
+		response.send(`Hello World Expresss!`);
 	}
 	create(request: Request, response: Response) {
 		const bodySchema = z.object({
-			name: z.string(),
-			price: z.number(),
-		})
+			name: z
+				.string({ required_error: 'nome e requerido' })
+				.trim()
+				.min(6),
+			price: z
+				.number()
+				// .nullish()
+				.positive(),
+		});
 
-		const { name, price } = bodySchema.parse(request.body)
+		const { name, price } = bodySchema.parse(request.body);
 		//throw new Error("Erro ao tentar criar produto")
 
 		// throw new AppError('Erro ao tentar criar um produto!')
@@ -31,7 +36,7 @@ export class ProductsController {
 		// response.send(`Nome do Produto é : ${name} Custa ${price}`) dessa forma a resposta
 		// chega como um texto
 
-		response.status(201).json({ name, price })
+		response.status(201).json({ name, price });
 		// dessa forma ja trasforma a res em json
 	}
 }
