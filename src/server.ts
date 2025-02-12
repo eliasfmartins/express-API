00c5dc8cf8784054a4b5998f65f2e8f579f22d5b
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-02-12 08:43:29 -0300

    feat: criando class para tratamento de erros e implementando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 
 import { routes } from './routes'
+
+import { AppError } from './utils/AppError'
 
 const PORT = 3333
 
@@ -9,5 +11,14 @@ const app = express()
 app.use(express.json()) //dizendo que vc vai usar o json dentro do express
 
 app.use(routes)
+
+// tratamento de erros caso ao contrario vai aparecer um erro nada amigavel
+app.use((error: any, request: Request, response: any, _: NextFunction) => {
+
+	if (error instanceof AppError) {
+		return response.status(error.statusCode).json({ message: error.message })
+	}
+	response.status(500).json({ message: "Erro Na Requisição" })
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
